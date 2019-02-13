135e206f8ef43e1ab2f6737b2624308348ac55fb
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-13 04:00:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,13 +3,11 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  sorted_array = array.sort
-  return sorted_array
+  array.sort
 end
 
 def using_reverse(array)
-  reversed_array = array.reverse
-  return reversed_array
+  array.reverse
 end
 
 def using_first(array)
